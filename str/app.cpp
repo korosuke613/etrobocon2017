90e257f0f384d762330fcc54e2150711b8370d27
Morichan <nintendo698@gmail.com>
2017-06-15 08:13:45 +0100

    01走行するようにはできた

--- str/app.cpp
+++ str/app.cpp
@@ -10,7 +10,7 @@
 
 #include "ev3api.h"
 #include "app.h"
-#include "balancer.h"
+#include "app/SonarAlert.h"
 
 #if defined(BUILD_MODULE)
 #include "module_cfg.h"
@@ -30,15 +30,16 @@
  * センサー、モーターの接続を定義します
  */
 static const sensor_port_t
-    touch_sensor    = EV3_PORT_1,
-    sonar_sensor    = EV3_PORT_2,
-    color_sensor    = EV3_PORT_3,
-    gyro_sensor     = EV3_PORT_4;
+    touch_sensor = EV3_PORT_4,
+    sonar_sensor = EV3_PORT_3,
+    color_sensor = EV3_PORT_2,
+    gyro_sensor  = EV3_PORT_1;
 
 static const motor_port_t
-    left_motor      = EV3_PORT_C,
-    right_motor     = EV3_PORT_B,
-    tail_motor      = EV3_PORT_A;
+    left_motor   = EV3_PORT_C,
+    right_motor  = EV3_PORT_B,
+    lifter       = EV3_PORT_A,
+    emoter       = EV3_PORT_D;
 
 static int      bt_cmd = 0;     /* Bluetoothコマンド 1:リモートスタート */
 static FILE     *bt = NULL;     /* Bluetoothファイルハンドル */
@@ -65,19 +66,12 @@ static FILE     *bt = NULL;     /* Bluetoothファイルハンドル */
 #define CALIB_FONT_WIDTH (6/*TODO: magic number*/)
 #define CALIB_FONT_HEIGHT (8/*TODO: magic number*/)
 
-/* 関数プロトタイプ宣言 */
-static int sonar_alert(void);
-static void tail_control(signed int angle);
-
-#include "BalancerCpp.h"        // <1>
-Balancer balancer;              // <1>
-
 /* メインタスク */
 void main_task(intptr_t unused)
 {
+    SonarAlert sonarAlert( 10, 4 );
     signed char forward;      /* 前後進命令 */
     signed char turn;         /* 旋回命令 */
-    signed char pwm_L, pwm_R; /* 左右モータPWM出力 */
 
     /* LCD画面表示 */
     ev3_lcd_fill_rect(0, 0, EV3_LCD_WIDTH, EV3_LCD_HEIGHT, EV3_LCD_WHITE);
@@ -92,8 +86,9 @@ void main_task(intptr_t unused)
     /* モーター出力ポートの設定 */
     ev3_motor_config(left_motor, LARGE_MOTOR);
     ev3_motor_config(right_motor, LARGE_MOTOR);
-    ev3_motor_config(tail_motor, LARGE_MOTOR);
-    ev3_motor_reset_counts(tail_motor);
+    ev3_motor_config(lifter, LARGE_MOTOR);
+    ev3_motor_config(emoter, LARGE_MOTOR);
+    ev3_motor_reset_counts(lifter);
 
     /* Open Bluetooth file */
     bt = ev3_serial_open_file(EV3_SERIAL_BT);
@@ -107,8 +102,6 @@ void main_task(intptr_t unused)
     /* スタート待機 */
     while(1)
     {
-        tail_control(TAIL_ANGLE_STAND_UP); /* 完全停止用角度に制御 */
-
         if (bt_cmd == 1)
         {
             break; /* リモートスタート */
@@ -128,7 +121,6 @@ void main_task(intptr_t unused)
 
     /* ジャイロセンサーリセット */
     ev3_gyro_sensor_reset(gyro_sensor);
-    balancer.init(GYRO_OFFSET);                // <1>
 
     ev3_led_set_color(LED_GREEN); /* スタート通知 */
 
@@ -137,14 +129,9 @@ void main_task(intptr_t unused)
     */
     while(1)
     {
-        int32_t motor_ang_l, motor_ang_r;
-        int gyro, volt;
-
         if (ev3_button_is_pressed(BACK_BUTTON)) break;
 
-        tail_control(TAIL_ANGLE_DRIVE); /* バランス走行用角度に制御 */
-
-        if (sonar_alert() == 1) /* 障害物検知 */
+        if (sonarAlert.detectBarrier( 20 ) == 1) /* 障害物検知 */
         {
             forward = turn = 0; /* 障害物を検知したら停止 */
         }
@@ -153,46 +140,16 @@ void main_task(intptr_t unused)
             forward = 30; /* 前進命令 */
             if (ev3_color_sensor_get_reflect(color_sensor) >= (LIGHT_WHITE + LIGHT_BLACK)/2)
             {
-                turn =  20; /* 左旋回命令 */
+                turn =  10; /* 左旋回命令 */
             }
             else
             {
-                turn = -20; /* 右旋回命令 */
+                turn = -10; /* 右旋回命令 */
             }
         }
 
-        /* 倒立振子制御API に渡すパラメータを取得する */
-        motor_ang_l = ev3_motor_get_counts(left_motor);
-        motor_ang_r = ev3_motor_get_counts(right_motor);
-        gyro = ev3_gyro_sensor_get_rate(gyro_sensor);
-        volt = ev3_battery_voltage_mV();
-
-        /* 倒立振子制御APIを呼び出し、倒立走行するための */
-        /* 左右モータ出力値を得る */
-        balancer.setCommand(forward, turn);   // <1>
-        balancer.update(gyro, motor_ang_r, motor_ang_l, volt); // <2>
-        pwm_L = balancer.getPwmRight();       // <3>
-        pwm_R = balancer.getPwmLeft();        // <3>
-
-        /* EV3ではモーター停止時のブレーキ設定が事前にできないため */
-        /* 出力0時に、その都度設定する */
-        if (pwm_L == 0)
-        {
-             ev3_motor_stop(left_motor, true);
-        }
-        else
-        {
-            ev3_motor_set_power(left_motor, (int)pwm_L);
-        }
-
-        if (pwm_R == 0)
-        {
-             ev3_motor_stop(right_motor, true);
-        }
-        else
-        {
-            ev3_motor_set_power(right_motor, (int)pwm_R);
-        }
+        ev3_motor_set_power(left_motor, forward + turn);
+        ev3_motor_set_power(right_motor, forward - turn);
 
         tslp_tsk(4); /* 4msec周期起動 */
     }
@@ -203,70 +160,6 @@ void main_task(intptr_t unused)
     fclose(bt);
 
     ext_tsk();
-}
-
-//*****************************************************************************
-// 関数名 : sonar_alert
-// 引数 : 無し
-// 返り値 : 1(障害物あり)/0(障害物無し)
-// 概要 : 超音波センサによる障害物検知
-//*****************************************************************************
-static int sonar_alert(void)
-{
-    static unsigned int counter = 0;
-    static int alert = 0;
-
-    signed int distance;
-
-    if (++counter == 40/4) /* 約40msec周期毎に障害物検知  */
-    {
-        /*
-         * 超音波センサによる距離測定周期は、超音波の減衰特性に依存します。
-         * NXTの場合は、40msec周期程度が経験上の最短測定周期です。
-         * EV3の場合は、要確認
-         */
-        distance = ev3_ultrasonic_sensor_get_distance(sonar_sensor);
-        if ((distance <= SONAR_ALERT_DISTANCE) && (distance >= 0))
-        {
-            alert = 1; /* 障害物を検知 */
-        }
-        else
-        {
-            alert = 0; /* 障害物無し */
-        }
-        counter = 0;
-    }
-
-    return alert;
-}
-
-//*****************************************************************************
-// 関数名 : tail_control
-// 引数 : angle (モータ目標角度[度])
-// 返り値 : 無し
-// 概要 : 走行体完全停止用モータの角度制御
-//*****************************************************************************
-static void tail_control(signed int angle)
-{
-    float pwm = (float)(angle - ev3_motor_get_counts(tail_motor))*P_GAIN; /* 比例制御 */
-    /* PWM出力飽和処理 */
-    if (pwm > PWM_ABS_MAX)
-    {
-        pwm = PWM_ABS_MAX;
-    }
-    else if (pwm < -PWM_ABS_MAX)
-    {
-        pwm = -PWM_ABS_MAX;
-    }
-
-    if (pwm == 0)
-    {
-        ev3_motor_stop(tail_motor, true);
-    }
-    else
-    {
-        ev3_motor_set_power(tail_motor, (signed char)pwm);
-    }
 }
 
 //*****************************************************************************
