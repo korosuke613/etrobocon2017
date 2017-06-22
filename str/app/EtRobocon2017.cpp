92309669b8819e11d5cb7f2588791ddcf66d8530
Morichan <nintendo698@gmail.com>
2017-06-22 11:26:59 +0100

    CのAPIからC++のAPIに変更。またEtRobocon2017クラス内のマクロ定義を全削除

--- str/app/EtRobocon2017.cpp
+++ str/app/EtRobocon2017.cpp
@@ -1,54 +1,27 @@
 #include "EtRobocon2017.h"
 
+EtRobocon2017::EtRobocon2017():
+    touchSensor( PORT_4 ), colorSensor( PORT_2 )
+{
+    light_white = 60;
+    light_black = 0;
+}
+
 void EtRobocon2017::start( int bluetooth_command )
 {
-    SonarAlert sonarAlert( 10, 4 );
-    signed char forward;      /* 前後進命令 */
-    signed char turn;         /* 旋回命令 */
+    SonarAlert sonarAlert( 4 );
+    int8_t forward = 0;      /* 前後進命令 */
+    int8_t turn = 0;         /* 旋回命令 */
 
-    /* LCD画面表示 */
-    ev3_lcd_fill_rect(0, 0, EV3_LCD_WIDTH, EV3_LCD_HEIGHT, EV3_LCD_WHITE);
-    ev3_lcd_draw_string("EV3way-ET sample_c4", 0, CALIB_FONT_HEIGHT*1);
 
-    /* センサー入力ポートの設定 */
-    ev3_sensor_config(sonar_sensor, ULTRASONIC_SENSOR);
-    ev3_sensor_config(color_sensor, COLOR_SENSOR);
-    ev3_color_sensor_get_reflect(color_sensor); /* 反射率モード */
-    ev3_sensor_config(touch_sensor, TOUCH_SENSOR);
-    ev3_sensor_config(gyro_sensor, GYRO_SENSOR);
-    /* モーター出力ポートの設定 */
-    ev3_motor_config(left_motor, LARGE_MOTOR);
-    ev3_motor_config(right_motor, LARGE_MOTOR);
-    ev3_motor_config(lifter, LARGE_MOTOR);
-    ev3_motor_config(emoter, LARGE_MOTOR);
-    ev3_motor_reset_counts(lifter);
-
-    ev3_led_set_color(LED_ORANGE); /* 初期化完了通知 */
-
-    /* スタート待機 */
-    while(1)
-    {
-        if (bluetooth_command == 1)
-        {
-            break; /* リモートスタート */
-        }
-
-        if (ev3_touch_sensor_is_pressed(touch_sensor) == 1)
-        {
-            break; /* タッチセンサが押された */
-        }
-
-        tslp_tsk(10); /* 10msecウェイト */
-    }
-
-    /* 走行モーターエンコーダーリセット */
-    ev3_motor_reset_counts(left_motor);
-    ev3_motor_reset_counts(right_motor);
-
-    /* ジャイロセンサーリセット */
-    ev3_gyro_sensor_reset(gyro_sensor);
+    waitStarter( bluetooth_command );
 
     ev3_led_set_color(LED_GREEN); /* スタート通知 */
+
+    Walker walker;
+    Lifter lifter;
+    Emoter emoter;
+    // bool isUpped = false;
 
     /**
     * Main loop for the self-balance control algorithm
@@ -57,28 +30,53 @@ void EtRobocon2017::start( int bluetooth_command )
     {
         if (ev3_button_is_pressed(BACK_BUTTON)) break;
 
-        if (sonarAlert.detectBarrier( 20 ) == 1) /* 障害物検知 */
+        if ( sonarAlert.detectBarrier( 20 ) == 1 ) // 障害物検知
         {
-            forward = turn = 0; /* 障害物を検知したら停止 */
+            forward = turn = 0; // 障害物を検知したら停止
         }
         else
         {
-            forward = 30; /* 前進命令 */
-            if (ev3_color_sensor_get_reflect(color_sensor) >= (LIGHT_WHITE + LIGHT_BLACK)/2)
+            forward = 30; // 前進命令
+            if ( colorSensor.getBrightness() >= (light_white + light_black)/2)
             {
-                turn =  10; /* 左旋回命令 */
+                turn =  10; // 左旋回命令
             }
             else
             {
-                turn = -10; /* 右旋回命令 */
+                turn = -10; // 右旋回命令
             }
         }
 
-        ev3_motor_set_power(left_motor, forward + turn);
-        ev3_motor_set_power(right_motor, forward - turn);
+        walker.run( forward, turn );
 
-        tslp_tsk(4); /* 4msec周期起動 */
+        // if( isUpped ) {
+        //     lifter.liftDown();
+        //     isUpped = false;
+        // } else {
+        //     lifter.liftUp();
+        //     isUpped = true;
+        // }
+
+        tslp_tsk(4); // 4msec周期起動
     }
-    ev3_motor_stop(left_motor, false);
-    ev3_motor_stop(right_motor, false);
+}
+
+void EtRobocon2017::waitStarter( int bluetooth_command )
+{
+    /* スタート待機 */
+    while(1)
+    {
+        if (bluetooth_command == 1)
+        {
+            break; /* リモートスタート */
+        }
+
+        if ( touchSensor.isPressed()== 1)
+        {
+            tslp_tsk(500);
+            break; /* タッチセンサが押された */
+        }
+
+        tslp_tsk(10); /* 10msecウェイト */
+    }
 }
