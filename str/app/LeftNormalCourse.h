bb0eaf919e904bd2fcdc20cdd3c335db3bdd2b11
Futa Hirakoba <korosuke613613@gmail.com>
2017-07-19 19:18:50 +0900

    右・左コース用のNormalCourseクラスを継承したクラスを作成

---
+++ str/app/LeftNormalCourse.h
@@ -0,0 +1,11 @@
+#ifndef __LEFTNORMALCOURSE__
+#define __LEFTNORMALCOURSE__
+
+#include "NormalCourse.h"
+
+using namespace ev3api;
+
+class LeftNormalCourse : public NormalCourse
+{
+    
+}
---
+++ str/app/RightNormalCourse.h
@@ -0,0 +1,11 @@
+#ifndef __RIGHTNORMALCOURSE__
+#define __RIGHTNORMALCOURSE__
+
+#include "NormalCourse.h"
+
+using namespace ev3api;
+
+class RightNormalCourse : public NormalCourse
+{
+    
+}
