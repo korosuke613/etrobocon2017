bb25feb2f41f758c679db9d387f0326484830fd4
Futa Hirakoba <korosuke613613@gmail.com>
2017-08-09 20:41:52 +0900

    LeftNormalCourseクラスから不要なコードを除去

--- str/apps/src/LeftNormalCourse.cpp
+++ str/apps/src/LeftNormalCourse.cpp
@@ -5,22 +5,9 @@ LeftNormalCourse::LeftNormalCourse(){
     old_status = LeftStatus::STRAIGHT;
 
     ev3_speaker_set_volume(100);
-    FILE *fp;
-
-    if ((fp = fopen("test_left.csv", "w")) == NULL) {
-            printf("file open error!!\n");
-            return;
-    }
-    fprintf(fp, "light_value, speed, forward, turn, speedPid, TurnPid\n");
-    fclose(fp);
 }
 
 void LeftNormalCourse::runNormalCourse(void){
-    FILE *fp;
-    if ((fp = fopen("test_left.csv", "a")) == NULL) {
-        printf("file open error!!\n");
-        return;
-    }
 	while ( 1 ) {
         sl.update();
         sl.writing_current_coordinates();
@@ -38,16 +25,7 @@ void LeftNormalCourse::runNormalCourse(void){
 		lineTracer.runLine();
 		if (ev3_button_is_pressed(BACK_BUTTON)) break;
         if (status == LeftStatus::STOP) break;
-        fprintf(fp, "%d, %d, %d, %d, %d\n",
-        lineTracer.speedControl.speed_value_all, 
-        forward,
-        lineTracer.turn,
-        (int)lineTracer.speedControl.get_output(),
-        (int)lineTracer.turnControl.get_output()
-        );
 	}
-
-    fclose(fp);
 }
 
 void LeftNormalCourse::statusCheck(){
@@ -68,18 +46,12 @@ void LeftNormalCourse::goStraight(int8_t forward_value){
     lineTracer.setForward(forward_value);
     lineTracer.speedControl.setPid ( 2.0, 4.8, 0.024, 150.0 );
     lineTracer.turnControl.setPid ( 2.0, 1.0, 0.048, 40.0 );
-    char msg[32];
-    sprintf(msg, "Speed_cm/s: %d", lineTracer.speedControl.speed_value_all); 
-    msg_f(msg, 4);
 }
 
 void LeftNormalCourse::goStraightSlow(int8_t forward_value){
     lineTracer.setForward(forward_value);
     lineTracer.speedControl.setPid ( 2.0, 2.0, 0.024, 120.0 );
     lineTracer.turnControl.setPid ( 2.0, 1.0, 0.048, 40.0 );
-    char msg[32];
-    sprintf(msg, "Speed_cm/s: %d", lineTracer.speedControl.speed_value_all); 
-    msg_f(msg, 4);
 }
 
 void LeftNormalCourse::goCurveRight(int8_t forward_value){
@@ -87,27 +59,18 @@ void LeftNormalCourse::goCurveRight(int8_t forward_value){
     lineTracer.speedControl.setPid ( 4.0, 0.8, 0.08, 100.0 );
     lineTracer.turnControl.setPid ( 4.0, 2.0, 0.1, 35.0 );
     //lineTracer.turnControl.setPid ( 4.0, 2.0, 0.096, 40.0 );
-    char msg[32];
-    sprintf(msg, "Speed_cm/s: %d", lineTracer.speedControl.speed_value_all); 
-    msg_f(msg, 4);
 }
 
 void LeftNormalCourse::goCurveLeftShort(int8_t forward_value){
     lineTracer.setForward(forward_value);
     lineTracer.speedControl.setPid ( 4.0, 0.8, 0.1, 130.0 );
     lineTracer.turnControl.setPid ( 2.0, 0.5, 0.048, 35.0 );
-    char msg[32];
-    sprintf(msg, "Speed_cm/s: %d", lineTracer.speedControl.speed_value_all); 
-    msg_f(msg, 4);
 }
 
 void LeftNormalCourse::goCurveLeft(int8_t forward_value){
     lineTracer.setForward(forward_value);
     lineTracer.speedControl.setPid ( 4.0, 0.8, 0.1, 100.0 );
     lineTracer.turnControl.setPid ( 4.0, 2.0, 0.096, 35.0 );
-    char msg[32];
-    sprintf(msg, "Speed_cm/s: %d", lineTracer.speedControl.speed_value_all); 
-    msg_f(msg, 4);
 }
 
 void LeftNormalCourse::stop(){
