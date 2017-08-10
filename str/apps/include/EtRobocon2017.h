12c60e676c3888b1278012085ff2f0b079b96b65
Futa Hirakoba <korosuke613613@gmail.com>
2017-08-10 16:34:48 +0900

    LeftNormalCourseクラスの処理をLeftCourseクラスの中に委譲

--- str/apps/include/EtRobocon2017.h
+++ str/apps/include/EtRobocon2017.h
@@ -2,15 +2,11 @@
 #define __ETROBOCON2017__
 
 #include "ev3api.h"
-#include "Walker.h"
 #include "TouchSensor.h"
-#include "ColorSensor.h"
 #include "SonarAlert.h"
 #include "Lifter.h"
 #include "Emoter.h"
-#include "LeftNormalCourse.h"
-#include "RightNormalCourse.h"
-#include "SelfLocalization.h"
+#include "LeftCourse.h"
 
 
 using namespace ev3api;
@@ -23,13 +19,9 @@ public:
     void loop();
 
 private:
-	Walker walker;
     TouchSensor touchSensor;
-    ColorSensor colorSensor;
     int8_t light_white;
     int8_t light_black;
-    /* 自己位置推定 インスタンス 初期化*/
-    SelfLocalization sl;
 };
 
 #endif
