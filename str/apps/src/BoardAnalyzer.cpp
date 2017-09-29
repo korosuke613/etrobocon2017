e03078c3bf10b62d54be1a30d344940261a24291
rewish <rewish@gmail.com>
2017-09-29 22:26:08 +0900

    左凹四角形上の交換パターンを検出するhasExchangePatternOnLeftSquareメソッド完成

--- str/apps/src/BoardAnalyzer.cpp
+++ str/apps/src/BoardAnalyzer.cpp
@@ -11,13 +11,13 @@ BoardAnalyzer::~BoardAnalyzer()
 }
 
 // ブロックの配置の入力
-void BoardAnalyzer::set(int *initPositions)
+void BoardAnalyzer::set(int8_t *initPositions)
 {
 	positions = initPositions;
 }
 
 // 左凹四角形中のブロックの個数
-int BoardAnalyzer::getInLeftSquareCount()
+int8_t BoardAnalyzer::getInLeftSquareCount()
 {
 	int leftSquareCount = 0;
 	
@@ -36,19 +36,23 @@ int BoardAnalyzer::getInLeftSquareCount()
 	return leftSquareCount;
 }
 
+// 左凹四角形上の交換パターンを検出
 bool BoardAnalyzer::hasExchangePatternOnLeftSquare()
 {
-	// 
+	bool isExchangePattern = false;
+	
+	// 初期位置の中で、左凹四角形上にあるものを取得
 	for(int i=0; i<5; i++)
 	{
 		for(int j=0; j<5; j++)
 		{
-			if()
+			if(positions[i] == leftSquare[j] && positions[j] == leftSquare[i])
 			{
-			
+				isExchangePattern = true;
 			}
 		}
 	}
-	return ;
+	
+	return isExchangePattern;
 }
 
--- str/test/BoardAnalyzerTest.cpp
+++ str/test/BoardAnalyzerTest.cpp
@@ -6,7 +6,7 @@
 TEST(LeftSquareCountTest, return0SetBlack9Red13Yellow9Blue6Green14)
 {
 	BoardAnalyzer analyzer;
-	int initPositions[5] = {8,13,9,6,14};// 黒 赤 黄 青 緑
+	int8_t initPositions[5] = {8,13,9,6,14};// 黒 赤 黄 青 緑
 	
 	// 初期位置の入力
 	analyzer.set(initPositions);
@@ -19,7 +19,7 @@ TEST(LeftSquareCountTest, return0SetBlack9Red13Yellow9Blue6Green14)
 TEST(LeftSquareCountTest, return3SetBlack1Red2Yellow3Blue4Green5)
 {
 	BoardAnalyzer analyzer;
-	int initPositions[5] = {1,2,3,4,5};// 黒 赤 黄 青 緑
+	int8_t initPositions[5] = {1,2,3,4,5};// 黒 赤 黄 青 緑
 	
 	// 初期位置の入力
 	analyzer.set(initPositions);
@@ -32,7 +32,7 @@ TEST(LeftSquareCountTest, return3SetBlack1Red2Yellow3Blue4Green5)
 TEST(LeftSquareCountTest, return2SetBlack5Red8Yellow10Blue12Green6)
 {
 	BoardAnalyzer analyzer;
-	int initPositions[5] = {5,8,10,12,6};// 黒 赤 黄 青 緑
+	int8_t initPositions[5] = {5,8,10,12,6};// 黒 赤 黄 青 緑
 	
 	// 初期位置の入力
 	analyzer.set(initPositions);
@@ -42,14 +42,75 @@ TEST(LeftSquareCountTest, return2SetBlack5Red8Yellow10Blue12Green6)
 	
 }
 
-TEST(ExchangePatternTest, return1and2SetBlack10Red2Yellow8Blue1Green5)
+// 左凹四角形上の交換パターンの検出テスト
+TEST(LeftSquareExchangePatternTest, returnTrueSetBlack10Red2Yellow8Blue1Green5)
 {
 	BoardAnalyzer analyzer;
-	int initPositions[5] = {10,2,8,1,5};
+	int8_t initPositions[5] = {10,2,8,1,5};
 	
 	// 初期位置の入力
 	analyzer.set(initPositions);
 	
 	// 交換パターンの検出
-	ASSERT_EQ( analyzer.getExchangePattern());
+	ASSERT_EQ( analyzer.hasExchangePatternOnLeftSquare(), true);
+}
+
+TEST(LeftSquareExchangePatternTest, returnFalseSetBlack1Red2Yellow3Blue4Green5)
+{
+	BoardAnalyzer analyzer;
+	int8_t initPositions[5] = {1,2,3,4,5};
+	
+	// 初期位置の入力
+	analyzer.set(initPositions);
+	
+	// 交換パターンの検出
+	ASSERT_EQ( analyzer.hasExchangePatternOnLeftSquare(), false);
+}
+
+TEST(LeftSquareExchangePatternTest, returnTrueSetBlack9Red4Yellow10Blue41Green5)
+{
+	BoardAnalyzer analyzer;
+	int8_t initPositions[5] = {9,4,10,14,5};
+	
+	// 初期位置の入力
+	analyzer.set(initPositions);
+	
+	// 交換パターンの検出
+	ASSERT_EQ( analyzer.hasExchangePatternOnLeftSquare(), true);
+}
+
+TEST(LeftSquareExchangePatternTest, returnTrueSetBlack13Red5Yellow1Blue6Green4)
+{
+	BoardAnalyzer analyzer;
+	int8_t initPositions[5] = {13,5,1,6,4};
+	
+	// 初期位置の入力
+	analyzer.set(initPositions);
+	
+	// 交換パターンの検出
+	ASSERT_EQ( analyzer.hasExchangePatternOnLeftSquare(), true);
+}
+
+TEST(LeftSquareExchangePatternTest, returnTrueSetBlack1Red12Yellow6Blue10Green2)
+{
+	BoardAnalyzer analyzer;
+	int8_t initPositions[5] = {1,12,6,10,2};
+	
+	// 初期位置の入力
+	analyzer.set(initPositions);
+	
+	// 交換パターンの検出
+	ASSERT_EQ( analyzer.hasExchangePatternOnLeftSquare(), true);
+}
+
+TEST(LeftSquareExchangePatternTest, returnFalseSetBlack8Red2Yellow1Blue5Green3)
+{
+	BoardAnalyzer analyzer;
+	int8_t initPositions[5] = {8,2,1,5,3};
+	
+	// 初期位置の入力
+	analyzer.set(initPositions);
+	
+	// 交換パターンの検出
+	ASSERT_EQ( analyzer.hasExchangePatternOnLeftSquare(), false);
 }
